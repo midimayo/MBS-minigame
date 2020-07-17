7200f904f35d807393a3e89bf7e192de5f4413ba
Midi <strawberrypyro@gmail.com>
2020-07-17 10:32:04 -0400

    Garden and First Item
    So the first garden scene is set up, character movement works. What doesn't work is the dialogue collision, not yet anyway. It responds but nothin pops up. We are workin on it.

---
+++ MBS New Life Mini Game/Assets/Codes/CollisionTriggersText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+//this script detects a collision between the player character and any gameobject tagged as "interactable"
+//when the collision is detected, it activates a UI Canvas, which is parented to the player character and shows dialogue
+//there are two dialogues which can be shown, dialogue1 and dialogue 2. these can we written within the inspector
+public class CollisionTriggersText : MonoBehaviour
+{
+    public Canvas DialogueBox;
+    private Text DialogueManager;
+    private int currentText;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        DialogueBox = DialogueBox.GetComponentInChildren<Canvas>();
+        DialogueBox.gameObject.SetActive(false);
+    }
+  
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print (collision.gameObject.tag);
+        if (collision.gameObject.tag == "interactable")
+        {
+            DialogueBox.gameObject.SetActive(true);
+
+         }
+    }
+}
